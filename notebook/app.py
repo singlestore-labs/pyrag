d1df65ca700066422195c512df2e7692b48d65b8
Yaroslav Demenskyi <ydemenskyi@singlestore.com>
2024-02-14 15:57:23 -0800

    s3_process added

--- notebook/app.py
+++ notebook/app.py
@@ -1,7 +1,10 @@
+import io
 import os
+from typing import Callable, Hashable, Optional
 import boto3
 import torch
 import numpy as np
+import pandas as pd
 import singlestoredb as s2
 from transformers import AutoModel, AutoTokenizer
 from dotenv import load_dotenv
@@ -11,13 +14,13 @@ from semantic_text_splitter import CharacterTextSplitter
 
 load_dotenv()
 
-connection_url = os.environ.get('DB_CONNECTION_URL')
+connection_url = os.environ.get('DB_CONNECTION_URL') or ''
 db_name = os.environ.get('DB_NAME') or connection_url.split('/')[-1] or 'pyreg'
 aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 aws_bucket_name = os.environ.get('AWS_BUCKET_NAME')
 
-db_connection = s2.connect(connection_url)
+# db_connection = s2.connect(connection_url)
 s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
 model_name = 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'
@@ -38,23 +41,73 @@ def split_text(text: str):
     return text_splitter.chunks(text, 2048)
 
 
-def s3_get_files():
-    try:
-        response = s3.list_objects_v2(Bucket=aws_bucket_name)
-        files = response.get('Contents')
+def get_file_extension(name: str):
+    return os.path.splitext(name)[1][1:]
 
-        if not files:
+
+def file_content_to_df(content: str, extension: str):
+    if extension == 'csv':
+        return pd.read_csv(io.StringIO(content))
+
+    raise ValueError('Unsupported file format')
+
+
+def process_df(df: pd.DataFrame, customize_row: Optional[Callable[[Hashable, pd.Series, pd.DataFrame], None]]):
+    for i, row in df.iterrows():
+        input = row.to_json()
+        df.at[i, 'input'] = input
+        embedding = create_embedding(input)
+        df.at[i, 'embedding'] = str(embedding.tolist())
+
+        if customize_row:
+            customize_row(i, row, df)
+
+    return df
+
+
+def insert_df(df: pd.DataFrame):
+    print(df)
+    return
+
+
+def s3_process():
+    def get_files():
+        try:
+            response = s3.list_objects_v2(Bucket=aws_bucket_name)
+            files = response.get('Contents')
+
+            if not files:
+                return []
+
+            return files
+        except Exception as e:
+            print(e)
             return []
 
-        return files
-    except:
-        return []
+    def get_file_content(key: str):
+        try:
+            obj = s3.get_object(Bucket=aws_bucket_name, Key=key)
+            content = obj['Body'].read().decode('utf-8')
+            return content
+        except Exception as e:
+            print(e)
+            return ''
+
+    for file in get_files()[1:]:
+        file_name = file['Key']
+        file_extention = get_file_extension(file_name)
+        file_content = get_file_content(file_name)
+        df = file_content_to_df(file_content, file_extention)
+
+        def customize_row(i: Hashable, row: pd.Series, df: pd.DataFrame):
+            df.at[i, 'created_at'] = file['LastModified']
+
+        df = process_df(df, customize_row)
+        insert_df(df)
 
 
-def s3_get_file_content(key: str):
-    try:
-        obj = s3.get_object(Bucket=aws_bucket_name, Key=key)
-        content = obj['Body'].read().decode('utf-8')
-        return content
-    except:
-        return ''
+def main():
+    s3_process()
+
+
+main()
