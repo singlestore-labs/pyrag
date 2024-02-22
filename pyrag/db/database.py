1fe6e0d369f81737282ea9164a34ee520536d070
Yaroslav Demenskyi <ydemenskyi@singlestore.com>
2024-02-21 16:26:11 -0800

    Database class updated

--- pyrag/db/database.py
+++ pyrag/db/database.py
@@ -1,3 +1,4 @@
+from typing import Dict, List, Tuple
 import singlestoredb as s2
 
 
@@ -7,6 +8,19 @@ class Database:
         self.connection = s2.connect(self.connection_url)
         self.cursor = self.connection.cursor
 
-    def create_table(self, table_name: str):
+    def create_table(self, table_name: str, columns: List[Tuple[str, str]]):
+        column_definitions = ', '.join([f"{col_name} {data_type}" for col_name, data_type in columns])
         with self.cursor() as cursor:
-            pass
+            cursor.execute(f'CREATE TABLE IF NOT EXISTS {table_name} ({column_definitions})')
+
+    def drop_table(self, table_name: str):
+        with self.cursor() as cursor:
+            cursor.execute(f'DROP TABLE IF EXISTS {table_name}')
+
+    def insert_values(self, table_name: str, values: List[Dict[str, str]]):
+        columns = ', '.join(values[0].keys())
+        placeholders = ', '.join(['%s'] * len(values[0]))
+        to_insert = [tuple(i.values()) for i in values]
+
+        with self.cursor() as cursor:
+            cursor.executemany(f'INSERT INTO {table_name} ({columns}) VALUES ({placeholders})', to_insert)
